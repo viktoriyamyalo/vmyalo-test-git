42b32a260947d22f103192e9db1422e6b7757726
Viktoriya Myalo <viktoriya.myalo.lystopad@gmail.com>
2018-02-03 18:37:13 +0200

    Worked on making the content customizable

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ function openRestorePasswordForm() {
 function closeRestorePasswordForm() {
   overlayRestorePasswordForm.style.display = "none";
 }
+
+function deleteLastParagraph(event) {
+  event.preventDefault();
+  var paragraphs = document.getElementsByTagName('p');
+  paragraphs[paragraphs.length - 1].remove();
+}
+
+function changeFontSize(num) {
+  var paragraphs = document.getElementsByTagName('p');
+  Array.prototype.forEach.call(paragraphs, function(p) {
+    p.style.fontSize = num + 'px'
+  });
+}
+
+function changeBackgroundColor(color) {
+  document.getElementById('main').style.backgroundColor = color;
+}
+
+function changeFontFamily(font) {
+  var paragraphs = document.getElementsByTagName('p');
+  Array.prototype.forEach.call(paragraphs, function(p) {
+    p.style.fontFamily = font;
+  });
+}
+
+function toggleSidebar() {
+  document.getElementById('sidebar').classList.toggle('sidebar-hidden');
+  document.getElementById('main').classList.toggle('main-sidebar-visible');
+}
+
+/* making the page responsive */
+
+document.getElementById('sidebar').style.marginTop = document.getElementsByTagName('header')[0].offsetHeight + 'px';
